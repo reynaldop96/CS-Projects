d817586049d8feea20cb38181ab9b7e52179cc6c
reynaldop96 <reynaldop96@gmail.com>
2016-09-30 13:15:14 -0600

    Computer Simulation project

--- World.java
+++ World.java
@@ -3,10 +3,20 @@ import java.util.List;
 import java.awt.geom.Point2D;
 
 /**
+ * This class is part of a project simulation containing classes Hex and TasmanianDevil. 
+ * The directions of the project were as follows: 
+ 	Write classes to complete a simulation of devils moving around a hex-grid world infecting 
+	each other with devil facial tumor disease (DFTD). A world will be specified by the width of the 
+	grid (the height should be chosen so that the world is roughly square in terms of Cartesian coordinates),
+	the total number of devils in it, and the number of devils initially infected with DFTD. Each devil moves 
+	around the world by randomly chosing a location on the grid and moving one hex closer to its target during 
+	each step of the simulation. When a devil reaches its goal, it randomly chooses another goal. When two 
+	devils are at the same location and one of them is infected with DFTD and the other is not, there is an 
+	80% chance that the other devil becomes infected (if more than two devils are at the same location, this 
+	interaction should be repeated for each pair of devils).
+	
+ * It simulates a (hexagonal) world in which the tasmanian devil lives in. 
  * A hex-grid world of Tasmanian Devils.
- *
- * @author Jim Glenn
- * @version 0.1 2015-09-28
  */
 
 public class World
