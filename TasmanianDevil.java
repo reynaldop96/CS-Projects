d45acb0a0e53087648aa4b322a3fcaa7ef5b38c4
reynaldop96 <reynaldop96@gmail.com>
2016-09-30 13:19:16 -0600

    Computer Simulation project

--- TasmanianDevil.java
+++ TasmanianDevil.java
@@ -1,6 +1,18 @@
-//this class is part of a three file project including classes Hex and World. 
-//It simulates a tasmanian devil inhabiting a hexagonal grid
-
+/**
+ * This class is part of a project simulation containing classes Hex and World. 
+ * The directions of the project were as follows: 
+ 	Write classes to complete a simulation of devils moving around a hex-grid world infecting 
+	each other with devil facial tumor disease (DFTD). A world will be specified by the width of the 
+	grid (the height should be chosen so that the world is roughly square in terms of Cartesian coordinates),
+	the total number of devils in it, and the number of devils initially infected with DFTD. Each devil moves 
+	around the world by randomly chosing a location on the grid and moving one hex closer to its target during 
+	each step of the simulation. When a devil reaches its goal, it randomly chooses another goal. When two 
+	devils are at the same location and one of them is infected with DFTD and the other is not, there is an 
+	80% chance that the other devil becomes infected (if more than two devils are at the same location, this 
+	interaction should be repeated for each pair of devils).
+ 
+This class simulates an individual tasmanian devil inhabiting a hex-grid world. 
+*/
 
 public class TasmanianDevil{
 
